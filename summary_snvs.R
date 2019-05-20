7407a374b6c8438267a6ee75b06097c81f5bcc0c
Nicola Casiraghi <42352553+ncasiraghi@users.noreply.github.com>
2019-05-20 11:08:20 +0200

    automatic summary for different filters

--- summary_snvs.R
+++ summary_snvs.R
@@ -1,30 +1,42 @@
-setwd("/elaborazioni/sharedCO/Abemus_data_analysis/NimbleGen_SeqCapEZ_Exome_v3_Plasma_IPM/Strelka2_mutation_load_tables")
 
-load("MutationLoadTable_Strelka2_20190427.RData")
-load("Table_SNVs_tissue_and_plasma_Strelka2_20190427.RData")
+mutation_load_tables_folder <- "/elaborazioni/sharedCO/Abemus_data_analysis/NimbleGen_SeqCapEZ_Exome_v3_Plasma_IPM/Strelka2_mutation_load_tables"
 
-pdf("summary_snvs.pdf", h = 219, w = 297, paper='A4r')
+setwd(mutation_load_tables_folder)
 
-layout(matrix(c(1,1,2,2,
-                1,1,2,2,
-                3,3,4,4,
-                3,3,4,4), nrow=4, byrow=TRUE))
+mlt_list <- grep(list.files(mutation_load_tables_folder,pattern = "MutationLoadTable_",full.names = T),pattern = "\\.RData$",value = T)
 
-par(pty="s")
+for(mlt in mlt_list){
+  message(mlt)
+  pattern <- gsub(basename(mlt),pattern = "MutationLoadTable_Strelka2_",replacement = "")
+  snt <- grep(list.files(mutation_load_tables_folder,pattern = "Table_SNVs_tissue_and_plasma_",full.names = T),pattern = pattern,value = T)
+  load(mlt)
+  load(snt)
+  
+  pdf(file = paste0("summary_snvs_",gsub(pattern,pattern = "\\.RData",replacement = ""),".pdf"), h = 219, w = 297, paper='A4r')
+  
+  layout(matrix(c(1,1,2,2,
+                  1,1,2,2,
+                  3,3,4,4,
+                  3,3,4,4), nrow=4, byrow=TRUE))
+  
+  par(pty="s")
+  
+  boxplot(n.snvs ~ class,data = final, varwidth=T, ylab="n.snvs")
+  boxplot(n.snvs_nonsyn ~ class, data = final, varwidth=T, ylab="n.snvs_nonsyn")
+  boxplot(n.snvs ~ Type, data = final, varwidth=T, ylab="n.snvs")
+  boxplot(n.snvs_nonsyn ~ Type, data = final, varwidth=T, ylab="n.snvs_nonsyn")
+  
+  library( hexbin )
+  plasma <- tabout[which(tabout$Type=="Plasma"),]
+  tissue <- tabout[which(tabout$Type=="Tissue"),]
+  
+  bin<-hexbin(x = plasma$af_control,y = plasma$af_case, xbins=50)
+  plot(bin,main="all snvs in plasma",xlab="af_control",ylab="af_case") 
+  
+  bin<-hexbin(x = tissue$af_control,y = tissue$af_case, xbins=50)
+  plot(bin,main="all snvs in tissue",xlab="af_control",ylab="af_case") 
+  
+  dev.off()
 
-boxplot(n.snvs ~ class,data = final, varwidth=T, ylab="n.snvs")
-boxplot(n.snvs_nonsyn ~ class, data = final, varwidth=T, ylab="n.snvs_nonsyn")
-boxplot(n.snvs ~ Type, data = final, varwidth=T, ylab="n.snvs")
-boxplot(n.snvs_nonsyn ~ Type, data = final, varwidth=T, ylab="n.snvs_nonsyn")
+}
 
-library( hexbin )
-plasma <- tabout[which(tabout$Type=="Plasma"),]
-tissue <- tabout[which(tabout$Type=="Tissue"),]
-
-bin<-hexbin(x = plasma$af_control,y = plasma$af_case, xbins=50)
-plot(bin,main="all snvs in plasma",xlab="af_control",ylab="af_case") 
-
-bin<-hexbin(x = tissue$af_control,y = tissue$af_case, xbins=50)
-plot(bin,main="all snvs in tissue",xlab="af_control",ylab="af_case") 
-
-dev.off()
